0bb71e70078ef56f46b42b863f98951c2b4c9795
Muhammad Malik <muhammad.malik@ecomexperts.io>
2024-05-24 00:09:06 +0500

    [FEATURE]: Replace additional page with settings page and update app configuration
    Updated navigation to include the Settings page instead of the Additional page. Removed `app.additional.jsx`, added `app.settings.jsx` with new content, and updated single quotes in `app.jsx`. Enhanced `shopify.app.toml` with comprehensive app configuration details.

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,17 +1,17 @@
-import { json } from "@remix-run/node";
-import { Link, Outlet, useLoaderData, useRouteError } from "@remix-run/react";
-import { boundary } from "@shopify/shopify-app-remix/server";
-import { AppProvider } from "@shopify/shopify-app-remix/react";
-import { NavMenu } from "@shopify/app-bridge-react";
-import polarisStyles from "@shopify/polaris/build/esm/styles.css?url";
-import { authenticate } from "../shopify.server";
+import { json } from '@remix-run/node';
+import { Link, Outlet, useLoaderData, useRouteError } from '@remix-run/react';
+import { boundary } from '@shopify/shopify-app-remix/server';
+import { AppProvider } from '@shopify/shopify-app-remix/react';
+import { NavMenu } from '@shopify/app-bridge-react';
+import polarisStyles from '@shopify/polaris/build/esm/styles.css?url';
+import { authenticate } from '../shopify.server';
 
-export const links = () => [{ rel: "stylesheet", href: polarisStyles }];
+export const links = () => [{ rel: 'stylesheet', href: polarisStyles }];
 
 export const loader = async ({ request }) => {
   await authenticate.admin(request);
 
-  return json({ apiKey: process.env.SHOPIFY_API_KEY || "" });
+  return json({ apiKey: process.env.SHOPIFY_API_KEY || '' });
 };
 
 export default function App() {
@@ -23,7 +23,7 @@ export default function App() {
         <Link to="/app" rel="home">
           Home
         </Link>
-        <Link to="/app/additional">Additional page</Link>
+        <Link to="/app/settings">Settings</Link>
       </NavMenu>
       <Outlet />
     </AppProvider>
